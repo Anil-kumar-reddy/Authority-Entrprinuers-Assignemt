7d13c6130ae2ee12f0f53c027686a7d61b8a6f9c
Anil kumar Reddy <anilkumarreddysenagapally@gmail.com>
2026-03-05 11:30:59 +0530

    adding frontend files

---
+++ src/Welcome.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Welcome() {
+
+  const username = localStorage.getItem("username");
+
+  return (
+
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+
+      <h1>Welcome, {username}!</h1>
+
+      <p>You have successfully logged in.</p>
+
+    </div>
+
+  );
+
+}
+
+export default Welcome;
